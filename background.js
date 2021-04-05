4f07b9c1c0e2649c5825d8de0ebaba9cf9313b2d
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-04-05 18:15:49 +0200

    cleanup

--- background.js
+++ background.js
@@ -1,40 +1,27 @@
 
 const extId = "stopOpen";
+
 let stopOpen = false;
+
 browser.browserAction.setBadgeBackgroundColor({color: "red"})
-browser.browserAction.setBadgeText({"text": "off"}); // menu permission
+browser.browserAction.setBadgeText({"text": "off"}); 
 
 browser.browserAction.onClicked.addListener((tab) => {
-	stopOpen = !stopOpen;
+	stopOpen = (!stopOpen);
 	if(stopOpen){
-		browser.browserAction.setBadgeText({"text": "on", "tabId": tab.id}); // menu permission
+		browser.browserAction.setBadgeText({"text": "on", "tabId": tab.id}); 
 		browser.browserAction.setBadgeBackgroundColor({color: "green"})
 	}else{
-		browser.browserAction.setBadgeText({"text": "off", "tabId": tab.id}); // menu permission
+		browser.browserAction.setBadgeText({"text": "off", "tabId": tab.id}); 
 		browser.browserAction.setBadgeBackgroundColor({color: "red"})
 	}
 });
-/* not required, since new windows will open a new tab ... and closing that will also close the window
-browser.windows.onCreated.addListener( async (window) => {
-	if(stopOpen) {
-		const wid = window.id;
-		await browser.windows.remove(wid);
-		browser.notifications.create(extId + wid, {
-			"type": "basic",
-			"iconUrl": browser.runtime.getURL("icon.png"),
-			"title": extId + ':: Window', 
-			"message":  'stopped a window from opening'
-		});
-	}
-});
-*/
 
 browser.runtime.onInstalled.addListener( () => {
 	stopOpen = false;
 	browser.browserAction.setBadgeBackgroundColor({color: "red"})
 	browser.browserAction.setBadgeText({"text": "off"}); 
 });
-
 
 browser.tabs.onCreated.addListener( async (tab) => {
 	if(stopOpen) {
@@ -43,8 +30,8 @@ browser.tabs.onCreated.addListener( async (tab) => {
 		browser.notifications.create(extId + tid, {
 			"type": "basic",
 			"iconUrl": browser.runtime.getURL("icon.png"),
-			"title": extId + ':: Tab', 
-			"message":  'stopped a tab from opening'
+			"title": extId, 
+			"message":  'stopped tab from opening'
 		});
 	}
 });
